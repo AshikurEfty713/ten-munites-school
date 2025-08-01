63273d08e4c117d61961f77883984ce690e9053e
unknown <ashikurefty713@gmail.com>
2025-08-01 11:46:50 +0600

    build version

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  
    images: {
     domains: ['cdn.10minuteschool.com',
       's3.ap-southeast-1.amazonaws.com'
     ],
+    unoptimized: true,
   },
+  output: "export",
 };
 
 export default nextConfig;
